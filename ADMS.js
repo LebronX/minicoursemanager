d7a1dd7901a8757c17a3f51aec9c92f2177897ec
LebronX <782813460@qq.com>
2018-03-24 13:20:33 +0800

    First try

---
+++ ADMS.js
@@ -0,0 +1,41 @@
+﻿添加管理员：
+Insert INTO MANAGER VALUES ('000','OKJB','000');
+
+
+MANAGER:
+
+添加老师：
+Insert INTO TEACHER VALUES ('001','OJBK','001');
+
+添加学生：
+Insert INTO STUDENT VALUES ('101','JBOK','101');
+
+TEACHER:
+
+添加课程：
+Insert INTO COURSE VALUES ('201','UML','001','60');
+
+删除课程：
+Delete FROM COURSE 
+WHERE course_id = '201'
+
+发布公告：
+Insert INTO INFO VALUES ('301','HELLO','HELLO_WORLD','001');
+
+删除公告：
+Delete FROM COURSE 
+WHERE course_id = '301'
+
+
+STUDENT:
+
+
+
+
+
+
+
+
+
+
+
